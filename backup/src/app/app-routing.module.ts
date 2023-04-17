7d083bc67eeff6ee333cf8b1a4ba7c67879ce14f
Day <116986604+Day-Drea@users.noreply.github.com>
2023-04-16 19:23:10 -0500

    import.path.lenguaje

--- backup/src/app/app-routing.module.ts
+++ backup/src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AsesorComponent } from './component/asesor/asesor.component';
 import { AsesorInsertarComponent } from './component/asesor/asesor-insertar/asesor-insertar.component';
 
+import { LenguajeComponent } from './component/lenguaje/lenguaje.component';
+import { LenguajeInsertarComponent } from './component/lenguaje/lenguaje-insertar/lenguaje-insertar.component';
+
 const routes: Routes = [
   {
     path:'asesores',component:AsesorComponent, children:[
       {
         path:'asesoresinsertar',component:AsesorInsertarComponent,
+      }
+    ]
+  },
+  {
+    path:'lenguajes',component:LenguajeComponent, children:[
+      {
+        path:'lenguajesinsertar',component:LenguajeInsertarComponent,
       }
     ]
   }
