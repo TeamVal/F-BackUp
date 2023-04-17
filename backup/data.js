94dca7ef7dc5468057d1141b82cd5515cb72ebac
Day <116986604+Day-Drea@users.noreply.github.com>
2023-04-16 19:21:11 -0500

    data.lenguaje

--- backup/data.js
+++ backup/data.js
@@ -33,6 +33,32 @@ module.exports = function () {
         correo:"lufe123@gmail.com",
         arch:"c://a",
       }
+    ],
+      lenguajes: [
+      {
+        id:1,
+        desc: "Lenguaje de programación de alto nivel y orientado a objetos",
+        abrev: ".j",
+        tipoLeng:"Java",
+      },
+      {
+        id:2,
+        desc: "lenguaje de programación de alto nivel, multi-paradigma y orientado a objetos",
+        abrev: ".c",
+        tipoLeng:"C++",
+      },
+      {
+        id:3,
+        desc: "se utiliza en una amplia variedad de aplicaciones, desde aplicaciones web y móviles hasta ciencia de datos y aprendizaje automáticoad",
+        abrev: ".py",
+        tipoLeng:"Python",
+      },
+      {
+        id:4,
+        desc: "lenguaje de programación de alto nivel, orientado a objetos, desarrollado por Microsoft en el año 2000",
+        abrev: ".py",
+        tipoLeng:"C#",
+      }
     ]
   }
   return data
