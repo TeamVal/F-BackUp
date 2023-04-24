c0845bc0ade252c0bfc69a79b8038781ec7937ed
u20201b270Tucta <105952736+u20201b270Tucta@users.noreply.github.com>
2023-04-24 11:32:46 -0500

    Update recurso-insertar.component.ts

--- backup/src/app/component/recurso/recurso-insertar/recurso-insertar.component.ts
+++ backup/src/app/component/recurso/recurso-insertar/recurso-insertar.component.ts
@@ -11,12 +11,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./recurso-insertar.component.css']
 })
 export class RecursoInsertarComponent implements OnInit {
+   id:number=0;
+  editar:boolean=false;
+  
   form: FormGroup = new FormGroup({});
   recur: recusosA = new recusosA();
   mensaje: string = '';
   constructor(private pS: RecursoAService, private router: Router) {}
 
   ngOnInit(): void {
+     this.route.params.subscribe((data:Params)=>{
+    this.id=data['id'];
+    this.editar=data['id'] !=null;
+     this.init();
+    });
     this.form = new FormGroup({
       id: new FormControl(),
       titulo: new FormControl(),
@@ -34,14 +42,41 @@ export class RecursoInsertarComponent implements OnInit {
     this.recur.url = this.form.value['url'];
     this.recur.tamano = this.form.value['tamano'];
     if (this.form.value['titulo'].length > 0) {
-      this.pS.insert(this.recur).subscribe((data) => {
+      if(this.editar){
+      this.pS.update(this.recur).subscribe(()=>{
+      this.pS.list().subscribe((data) => {
+     this.pS.setList(data);
+     });
+     })
+
+
+      } else{
+       this.pS.insert(this.recur).subscribe((data) => {
         this.pS.list().subscribe((data) => {
           this.pS.setList(data);
         });
-      });
+        });
+
+      }
+      
       this.router.navigate(['recursos']);
     } else {
       this.mensaje = 'Ingrese un titulo';
+    }
+  }
+  init() {
+    if (this.editar) {
+      this.pS.listId(this.id).subscribe((data) => {
+        this.form = new FormGroup({
+          id: new FormControl(data.id),
+          titulo: new FormControl(data.titulo),
+          tema: new FormControl(data.tema),
+          tipocontenido: new FormControl(data.tipocontenido),
+          url:new FormControl(data.url),
+          tamano:new FormControl(data.tamano),
+
+        });
+      });
     }
   }
 }
