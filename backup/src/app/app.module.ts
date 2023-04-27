08056effb65bcc643adf11b2adedddae4b3c098f
u20201b270Tucta <105952736+u20201b270Tucta@users.noreply.github.com>
2023-04-27 10:11:51 -0500

    Update app.module.ts

--- backup/src/app/app.module.ts
+++ backup/src/app/app.module.ts
@@ -38,6 +38,8 @@ import { EstudianteInsertarComponent } from './component/estudiante/estudiante-i
 import { RecursolistarComponent } from './component/recurso/recurso-lista/recurso-lista.component';
 import { RecursoComponent } from './component/recurso/recurso.component';
 import { RecursoInsertarComponent } from './component/recurso/recurso-insertar/recurso-insertar.component';
+import { RecursoDialogoComponent } from './component/recurso/recurso-lista/Recursos-Dialogo/Recurso-Dialogo.component';
+
 
 
 import { NavbarComponent } from './component/navbar/navbar.component';
@@ -59,7 +61,8 @@ import { NavbarComponent } from './component/navbar/navbar.component';
     RecursoComponent, 
     RecursolistarComponent, 
     RecursoInsertarComponent,
-    NavbarComponent
+    NavbarComponent,
+    RecursoDialogoComponent
    
   ],
   imports: [
