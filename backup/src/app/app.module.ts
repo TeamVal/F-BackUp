0179388931e3c95368f620078353ce2e9157b83c
u20201b270Tucta <105952736+u20201b270Tucta@users.noreply.github.com>
2023-04-27 02:42:57 -0500

    dialog-recursoc

--- backup/src/app/app.module.ts
+++ backup/src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AsesorComponent } from './component/asesor/asesor.component';
-import { AsesorListarComponent } from './component/asesor/asesor-listar/asesor-listar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -14,11 +11,25 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import {  MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { AsesorComponent } from './component/asesor/asesor.component';
+import { AsesorListarComponent } from './component/asesor/asesor-listar/asesor-listar.component';
 import { AsesorInsertarComponent } from './component/asesor/asesor-insertar/asesor-insertar.component';
+import { AsesorDialogoComponent } from './component/asesor/asesor-listar/asesor-dialogo/asesor-dialogo.component';
+
 
 import { LenguajeComponent } from './component/lenguaje/lenguaje.component';
 import { LenguajeListarComponent } from './component/lenguaje/lenguaje-listar/lenguaje-listar.component';
 import { LenguajeInsertarComponent } from './component/lenguaje/lenguaje-insertar/lenguaje-insertar.component';
+import { LenguajeDialogoComponent } from './component/lenguaje/lenguaje-listar/lenguaje-dialogo/lenguaje-dialogo.component';
+
 
 import { EstudianteComponent } from './component/estudiante/estudiante.component';
 import { EstudianteListarComponent } from './component/estudiante/estudiante-listar/estudiante-listar.component';
@@ -27,6 +38,9 @@ import { EstudianteInsertarComponent } from './component/estudiante/estudiante-i
 import { RecursolistarComponent } from './component/recurso/recurso-lista/recurso-lista.component';
 import { RecursoComponent } from './component/recurso/recurso.component';
 import { RecursoInsertarComponent } from './component/recurso/recurso-insertar/recurso-insertar.component';
+import { RecursoDialogoComponent } from './component/recurso/recurso-lista/Recursos-Dialogo/Recurso-Dialogo.component';
+
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -34,15 +48,19 @@ import { RecursoInsertarComponent } from './component/recurso/recurso-insertar/r
     AsesorComponent,
     AsesorListarComponent,
     AsesorInsertarComponent,
+    AsesorDialogoComponent,
     LenguajeComponent,
     LenguajeListarComponent,
     LenguajeInsertarComponent,
+    LenguajeDialogoComponent,
     EstudianteComponent,
     EstudianteListarComponent,
     EstudianteInsertarComponent,
     RecursoComponent, 
     RecursolistarComponent, 
-    RecursoInsertarComponent
+    RecursoInsertarComponent,
+    NavbarComponent,
+    RecursoDialogoComponent
   ],
   imports: [
     BrowserModule,
@@ -56,7 +74,14 @@ import { RecursoInsertarComponent } from './component/recurso/recurso-insertar/r
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatMenuModule,
+    MatListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
