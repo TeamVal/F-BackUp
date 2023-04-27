02b0fafef9297456f52009dfa3585d9f99ba556b
Geanella12 <91570525+Geanella12@users.noreply.github.com>
2023-04-27 00:31:30 -0500

    edit estudiante

--- backup/src/app/app-routing.module.ts
+++ backup/src/app/app-routing.module.ts
@@ -9,11 +9,18 @@ import { LenguajeInsertarComponent } from './component/lenguaje/lenguaje-inserta
 import { EstudianteInsertarComponent } from './component/estudiante/estudiante-insertar/estudiante-insertar.component';
 import { EstudianteComponent } from './component/estudiante/estudiante.component';
 
+import { RecursoComponent } from './component/recurso/recurso.component';
+import { RecursoInsertarComponent } from './component/recurso/recurso-insertar/recurso-insertar.component';
+
+
 const routes: Routes = [
   {
     path:'asesores',component:AsesorComponent, children:[
       {
         path:'asesoresinsertar',component:AsesorInsertarComponent,
+      },
+      {
+        path:'edicion/:id',component:AsesorInsertarComponent,
       }
     ]
   },
@@ -21,6 +28,9 @@ const routes: Routes = [
     path:'lenguajes',component:LenguajeComponent, children:[
       {
         path:'lenguajesinsertar',component:LenguajeInsertarComponent,
+      },
+      {
+        path:'edicion/:id',component:LenguajeInsertarComponent,
       }
     ]
   },
@@ -32,8 +42,25 @@ const routes: Routes = [
         path: 'estudiantesinsertar',
         component: EstudianteInsertarComponent,
       },
+      {
+         path:'edicion/:id',component:EstudianteInsertarComponent,
+      }
     ],
+  },
+  
+  
+  {
+    path:'recursos',
+    component:RecursoComponent,
+    children:[
+
+  {path:'recursosinsertar',component:RecursoInsertarComponent},
+  {path:'editar/:id',component:RecursoInsertarComponent}
+],
   }
+  
+  
+ 
 ];
 
 @NgModule({
